b27ebd0855f1e459ad41827cd56c4615c4962e43
Raul Camacho <rcamacho96@gmail.com>
2018-12-03 18:25:59 -0600

    add prompt

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
     this.updateTrack = this.updateTrack.bind(this)
     this.getPlaybackState = this.getPlaybackState.bind(this)
     this.startPoll = this.startPoll.bind(this)
+    this.checkSpotifyState = this.checkSpotifyState.bind(this)
     this.shadeColor2 = this.shadeColor2.bind(this)
   }
 
@@ -30,8 +31,10 @@ class App extends Component {
   }
 
   startPoll() {
-    this.getPlaybackState()
-    this.updateTrack()
+    if(this.state.isRunning) {
+      this.getPlaybackState()
+      this.updateTrack()
+    }
     setTimeout(this.startPoll, 500)
   }
 
@@ -63,6 +66,12 @@ class App extends Component {
     })
   }
 
+  checkSpotifyState = () => {
+    spotify.isRunning((err, isRunning) => {
+      this.setState({ isRunning: isRunning })
+    })
+  }
+
   shadeColor2(color, percent) {
     var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
     return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
@@ -70,29 +79,36 @@ class App extends Component {
 
   render() {
     return (
-      <div
-        id="container"
-        style={{background: `linear-gradient(${this.shadeColor2(this.state.bgColor, 0.3)}, ${this.state.bgColor}`}}>
-        <div id="viz-container">
-          <VinylSpinner
-            artwork_url={this.state.artwork}
-            playback={this.state.playback} />
-          {/* <VinylSpinner
-            artwork_url='test_art.jpeg'
-            playback='playing' /> */}
+      !this.state.isRunning ? (
+        <div id="prompt-container">
+          <h3>Ensure that Spotify is running, then click</h3>
+          <button onClick={this.checkSpotifyState}>here</button>
         </div>
-        <div id="controller-container" style={{color: this.shadeColor2(this.state.bgColor, 0.8)}}>
-          <DefaultController
-            track={this.state.track}
-            artist={this.state.artist}
-            playback={this.state.playback}
-          />
-          {/* <DefaultController
-            track='Love Story'
-            artist='Taylor Swift'
-          /> */}
+      ) : (
+        <div
+          id="container"
+          style={{background: `linear-gradient(${this.shadeColor2(this.state.bgColor, 0.3)}, ${this.state.bgColor}`}}>
+          <div id="viz-container">
+            <VinylSpinner
+              artwork_url={this.state.artwork}
+              playback={this.state.playback} />
+            {/* <VinylSpinner
+              artwork_url='test_art.jpeg'
+              playback='playing' /> */}
+          </div>
+          <div id="controller-container" style={{color: this.shadeColor2(this.state.bgColor, 0.8)}}>
+            <DefaultController
+              track={this.state.track}
+              artist={this.state.artist}
+              playback={this.state.playback}
+            />
+            {/* <DefaultController
+              track='Love Story'
+              artist='Taylor Swift'
+            /> */}
+          </div>
         </div>
-      </div>
+      )
     );
   }
 }
